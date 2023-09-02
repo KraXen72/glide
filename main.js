5f055abc3e127c7e946906242dfcac34ef213419
KraXen72 <dpelech1@gmail.com>
2023-09-02 13:17:10 +0200

    fix deps lmao

--- main.js
+++ main.js
@@ -53,6 +53,7 @@ if (typeof ls_containerObj !== 'undefined' && ls_containerObj !== null) {
 */
 
 const Container = Observable.from(containerObj)
+console.log(Container)
 Container.observe(changes => {
 	changes.forEach(change => {
 		let cl = containerElem.classList
